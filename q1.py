be828e5148c2d2e1f385ba217b16787d2de91531
maayan3330 <maayan3330@gmail.com>
2025-11-25 22:25:49 +0200

    1+2

---
+++ q1.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+if __name__ == "__main__":
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="f1_data",
+        port="3307",
+    )
+
+    cursor = mydb.cursor()
+
+    # We filter by Nationality = 'BRA' and select only Driver names.
+    cursor.execute("""
+        SELECT DISTINCT Driver
+        FROM drivers_updated
+        WHERE Nationality = 'BRA';
+    """)
+
+    print(', '.join(str(row) for row in cursor.fetchall()))
+
+    cursor.close()
+    mydb.close()
---
+++ q2.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+if __name__ == "__main__":
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="f1_data",
+        port="3307",
+    )
+
+    cursor = mydb.cursor()
+
+    cursor.execute("""
+        SELECT DISTINCT Driver
+        FROM drivers_updated
+        WHERE Nationality = 'ITA';
+    """)
+
+    print(', '.join(str(row) for row in cursor.fetchall()))
+
+    cursor.close()
+    mydb.close()
