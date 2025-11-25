d3694e78bb015d12849a3736ffd8ccac6511fce8
maayan3330 <maayan3330@gmail.com>
2025-11-26 00:18:44 +0200

    maby work

--- q3.py
+++ q3.py
@@ -12,7 +12,25 @@ if __name__ == "__main__":
     cursor = mydb.cursor()
 
     cursor.execute("""
-        
+      SELECT 
+    d.Winner,
+    MIN(f.Time) AS min_time
+FROM
+    (SELECT Winner, SUM(Laps) AS total_laps
+     FROM winners
+     WHERE YEAR(Date) = 2000
+     GROUP BY Winner
+     ORDER BY total_laps DESC
+     LIMIT 1) AS d
+JOIN winners AS w
+    ON w.Winner = d.Winner AND YEAR(w.Date) = 2000
+JOIN fastest_laps_updated AS f
+    ON f.Code = w.`Name Code`    -- ← זה המפתח האמיתי!
+WHERE f.year = 2000
+GROUP BY d.Winner;
+
+
+
     """)
 
     print(', '.join(str(row) for row in cursor.fetchall()))
