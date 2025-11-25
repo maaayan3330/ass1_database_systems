c6695eb741a80845d26c3c6dc4daa9b3c7584a35
maayan3330 <maayan3330@gmail.com>
2025-11-26 00:57:26 +0200

    also 4

--- q4.py
+++ q4.py
@@ -12,7 +12,18 @@ if __name__ == "__main__":
     cursor = mydb.cursor()
 
     cursor.execute("""
-        PUT_YOUR_QUERY
+        SELECT COUNT(*) AS wins_2001
+        FROM winners
+        WHERE YEAR(Date) = 2001
+        AND Car = (
+                SELECT Car
+                FROM winners
+                WHERE YEAR(Date) = 1999
+                GROUP BY Car
+                ORDER BY COUNT(*) DESC
+                LIMIT 1
+    );
+
     """)
 
     print(', '.join(str(row) for row in cursor.fetchall()))
