a1b6786a6713080b68e70731df7eb9c484755f32
maayan3330 <maayan3330@gmail.com>
2025-12-03 21:59:17 +0200

    9

--- q9.py
+++ q9.py
@@ -12,7 +12,28 @@ if __name__ == "__main__":
     cursor = mydb.cursor()
 
     cursor.execute("""
-     
+        WITH avg_points AS (
+        SELECT Nationality, AVG(PTS) AS avg_pts
+        FROM drivers_updated
+        GROUP BY Nationality
+        ),
+        min_fastest AS(
+        SELECT drivers_updated.Nationality, MIN(fastest_laps_updated.Time) AS min_time
+        FROM drivers_updated, fastest_laps_updated
+        WHERE drivers_updated.Driver = fastest_laps_updated.Driver
+        GROUP BY drivers_updated.Nationality
+        ),
+        latest_win AS(
+        SELECT drivers_updated.Nationality, MAX(winners.Date) AS latest
+        FROM drivers_updated, winners
+        WHERE drivers_updated.Driver = winners.Winner
+        GROUP BY drivers_updated.Nationality
+        )
+
+        SELECT avg_points.Nationality, avg_points.avg_pts, min_fastest.min_time, latest_win.latest
+        FROM avg_points
+        LEFT JOIN min_fastest USING (Nationality)
+        LEFT JOIN latest_win USING (Nationality)
     """)
 
     print(', '.join(str(row) for row in cursor.fetchall()))
